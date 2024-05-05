5a4d38c54fce7bc5f281fd2b825cf80931d1d871
DmitryIgnatev <dmitryignatevdev@gmail.com>
2024-05-05 21:55:56 +0400

    feat: add repository test

--- lib/src/features/github_search/domain/models/user.dart
+++ lib/src/features/github_search/domain/models/user.dart
@@ -24,4 +24,27 @@ class User {
       reposUrl: json['repos_url'] ?? '',
     );
   }
+
+  @override
+  bool operator ==(covariant User other) {
+    if (identical(this, other)) return true;
+  
+    return 
+      other.login == login &&
+      other.id == id &&
+      other.avatarUrl == avatarUrl &&
+      other.htmlUrl == htmlUrl &&
+      other.url == url &&
+      other.reposUrl == reposUrl;
+  }
+
+  @override
+  int get hashCode {
+    return login.hashCode ^
+      id.hashCode ^
+      avatarUrl.hashCode ^
+      htmlUrl.hashCode ^
+      url.hashCode ^
+      reposUrl.hashCode;
+  }
 }
--- lib/src/features/github_search/domain/models/user_search.dart
+++ lib/src/features/github_search/domain/models/user_search.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/foundation.dart';
+
 import 'package:github_search/src/features/github_search/domain/models/user.dart';
 
 class UserSearch {
@@ -14,4 +16,16 @@ class UserSearch {
         totalCount: json['total_count'] ?? 0,
         items: List.from(json['items']).map((e) => User.fromMap(e)).toList());
   }
+
+  @override
+  bool operator ==(covariant UserSearch other) {
+    if (identical(this, other)) return true;
+  
+    return 
+      other.totalCount == totalCount &&
+      listEquals(other.items, items);
+  }
+
+  @override
+  int get hashCode => totalCount.hashCode ^ items.hashCode;
 }
